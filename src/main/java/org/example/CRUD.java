0092e2255b33ab47684597a33fcf4738926cad96
Eduardll <fediks45@gmail.com>
2025-04-23 02:30:30 +0300

    kek

--- src/main/java/org/example/CRUD.java
+++ src/main/java/org/example/CRUD.java
@@ -1,4 +1,120 @@
 package org.example;
 
-public class CRUD {
+import org.hibernate.*;
+
+import javax.management.Query;
+import java.util.List;
+
+public class CRUD<T> {
+    private SessionFactory sessionFactory;
+    private final Class<T> user;
+
+    public CRUD(Class<T> user) {
+        this.user = user;
+        this.sessionFactory = HibernateUtil.getSessionFactory();
+    }
+
+    public void create(T entity){
+        Session session = getCurrentSession();
+        Transaction transaction = null;
+        try{
+            transaction = session.beginTransaction();
+            session.persist(entity);
+            transaction.commit();
+        }catch(Exception e){
+            if(transaction != null) {
+                transaction.rollback();
+            }
+            throw e;
+        }
+    }
+    public T readById(Long id){
+        Session session = getCurrentSession();
+        Transaction transaction = null;
+        try{
+            transaction = session.beginTransaction();
+            T entity = getCurrentSession().get(user,id);
+            transaction.commit();
+            return entity;
+        }catch (Exception e){
+            if(transaction != null) {
+                transaction.rollback();
+            }
+            throw e;
+        }
+    }
+    public List<T> readall(){
+        Session session = getCurrentSession();
+        Transaction transaction = null;
+        try{
+            transaction = session.beginTransaction();
+            String hql = "From " + user.getSimpleName();
+            List<T> resultList = getCurrentSession().createQuery(hql, user).getResultList();
+            transaction.commit();
+            return resultList;
+
+        }catch (Exception e){
+            if(transaction != null) {
+                transaction.rollback();
+            }
+            throw e;
+        }
+    }
+    public void update(T entity){
+        Session session = getCurrentSession();
+        Transaction transaction = null;
+        try{
+            transaction = session.beginTransaction();
+            session.merge(entity);
+            transaction.commit();
+
+        }catch (Exception e){
+            if(transaction != null) {
+                transaction.rollback();
+            }
+            throw e;
+        }
+    }
+    private void delete(T entity){
+        Session session = getCurrentSession();
+        Transaction transaction = null;
+        try{
+            transaction = session.beginTransaction();
+            session.remove(entity);
+            transaction.commit();
+
+        }catch (Exception e){
+            if(transaction != null) {
+                transaction.rollback();
+            }
+            throw e;
+        }
+    }
+    public void deleteById(Long id){
+        T entity = readById(id);
+        if (entity == null) {
+            throw new IllegalArgumentException("Данные по этому id: " + id + " не найдены");
+        }
+        delete(entity);
+    }
+    public int deleteAll(){
+        Session session = getCurrentSession();
+        Transaction transaction = null;
+        try{
+            transaction = session.beginTransaction();
+            String hql = "delete from " + user.getSimpleName();
+            int resultList = session.createQuery(hql).executeUpdate();
+            transaction.commit();
+            return resultList;
+        }catch(Exception e){
+            if(transaction != null) {
+                transaction.rollback();
+            }
+            throw e;
+        }
+    }
+    protected Session getCurrentSession() {
+        return sessionFactory.getCurrentSession();
+    }
+
 }
--- src/main/java/org/example/User.java
+++ src/main/java/org/example/User.java
@@ -1,4 +1,61 @@
 package org.example;
 
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String email;
+
+    @Column
+    private Integer age;
+
+    @Column(name = "created_date", updatable = false)
+    @CreationTimestamp
+    private LocalDateTime createdDate;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
