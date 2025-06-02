9e6a785f7ce62811bfbedf365730fb79bc048691
Eduardll <fediks45@gmail.com>
2025-06-03 00:21:46 +0300

    lolkek

--- src/main/java/org/example/spring/Controller.java
+++ src/main/java/org/example/spring/Controller.java
@@ -1,46 +1,86 @@
 package org.example.spring;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 @RestController
 @RequestMapping("/api/users")
+@Tag(name = "User API", description = "CRUD операции над пользователями")
 public class Controller {
 
     private final UserService userService;
 
-    @Autowired
     public Controller(UserService userService) {
         this.userService = userService;
     }
 
-    @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    public UserDTO createUser(@RequestBody UserDTO userDto) {
-        return userService.createUser(userDto);
-    }
-
-    @PutMapping("/{id}")
-    public UserDTO updateUser(@PathVariable Long id, @RequestBody UserDTO userDto) {
-        return userService.updateUser(id, userDto);
-    }
-
+    @Operation(summary = "Получить всех пользователей", description = "Возвращает список всех пользователей с ссылками на каждый ресурс")
     @GetMapping
-    public List<UserDTO> getAllUsers() {
-        return userService.getAllUsers();
+    public List<EntityModel<UserDTO>> getAllUsers() {
+        List<UserDTO> users = userService.getAllUsers();
+
+        return users.stream()
+                .map(user -> EntityModel.of(user,
+                        linkTo(methodOn(Controller.class).getUserById(user.getId())).withSelfRel(),
+                        linkTo(methodOn(Controller.class).getAllUsers()).withRel("all-users")))
+                .toList();
     }
 
+    @Operation(summary = "Получить пользователя по ID", responses = {
+            @ApiResponse(responseCode = "200", description = "Найден пользователь", content = @Content(schema = @Schema(implementation = UserDTO.class))),
+            @ApiResponse(responseCode = "404", description = "Пользователь не найден")
+    })
     @GetMapping("/{id}")
-    public UserDTO getUserById(@PathVariable Long id) {
-        return userService.getUsersById(id);
+    public EntityModel<UserDTO> getUserById(@PathVariable Long id) {
+        UserDTO user = userService.getUsersById(id);
+
+        return EntityModel.of(user,
+                linkTo(methodOn(Controller.class).getUserById(id)).withSelfRel(),
+                linkTo(methodOn(Controller.class).getAllUsers()).withRel("all-users"));
     }
 
+    @Operation(summary = "Создать нового пользователя", description = "Возвращает созданного пользователя с ссылкой на него")
+    @PostMapping
+    public ResponseEntity<EntityModel<UserDTO>> createUser(@RequestBody UserDTO userDto) {
+        UserDTO createdUser = userService.createUser(userDto);
+
+        EntityModel<UserDTO> model = EntityModel.of(createdUser,
+                linkTo(methodOn(Controller.class).getUserById(createdUser.getId())).withSelfRel());
+
+        return ResponseEntity.status(201).body(model);
+    }
+
+    @Operation(summary = "Обновить данные пользователя", description = "Обновляет существующего пользователя по ID")
+    @PutMapping("/{id}")
+    public ResponseEntity<EntityModel<UserDTO>> updateUser(
+            @PathVariable Long id,
+            @RequestBody UserDTO userDto) {
+
+        userDto.setId(id);
+        UserDTO updatedUser = userService.updateUser(id, userDto);
+
+        EntityModel<UserDTO> model = EntityModel.of(updatedUser,
+                linkTo(methodOn(Controller.class).getUserById(id)).withSelfRel(),
+                linkTo(methodOn(Controller.class).getAllUsers()).withRel("all-users"));
+
+        return ResponseEntity.ok(model);
+    }
+
+    @Operation(summary = "Удалить пользователя по ID")
     @DeleteMapping("/{id}")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void deleteUser(@PathVariable Long id) {
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
         userService.deleteUser(id);
+        return ResponseEntity.noContent().build();
     }
 }
